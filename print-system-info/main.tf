82152d38d822ee9ce6a9903c3554defaadb3172b
Ryan Uber <ru@ryanuber.com>
2018-10-08 12:10:15 -0700

    Fixups

--- print-system-info/main.tf
+++ print-system-info/main.tf
@@ -1,11 +1,31 @@
-data "external" "slow-data-source" {
+data "external" "sysinfo" {
   program = ["/bin/bash", "-c", <<EOF
-uptime
-echo "=== Memory ========================="
-free -m
-echo
-echo "=== CPU ============================"
-cat /proc/cpuinfo
+echo '{'
+
+echo -n '"uptime":"'
+uptime | tr -d '\n'
+echo -n '",'
+
+echo -n '"mem_total_kb":"'
+awk '/^MemTotal:/{print $2}' /proc/meminfo | tr -d '\n'
+echo '",'
+
+echo -n '"cpu_cores":"'
+awk /^processor/ /proc/cpuinfo| wc -l | tr -d '\n'
+echo '"'
+
+echo '}'
 EOF
 ]
+}
+
+resource "random_id" "random" {
+  keepers {
+    uuid = "${uuid()}"
+  }
+  byte_length = 8
+}
+
+output "sysinfo" {
+  value = "${data.external.sysinfo.result}"
 }
