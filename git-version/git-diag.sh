1772b53986ae3155e2fbd9a676d08e759b36ed18
Ryan Uber <ru@ryanuber.com>
2019-06-11 14:09:45 -0700

    Ignore errors

--- git-version/git-diag.sh
+++ git-version/git-diag.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
+set +e
+
 cat <<EOF
 ======= ~/.gitconfig contents:
-`cat ~/.gitconfig`
+`cat ~/.gitconfig || :`
 
 ======= GIT_SSH_COMMAND value
 ${GIT_SSH_COMMAND}
 
 ======= git version
-`git version`
+`git version || :`
 
 ======= /tmp/.ssh_private_key contents:
-`cat /tmp/.ssh_private_key`
+`cat /tmp/.ssh_private_key || :`
 EOF
 
 exit 1
