4cf99d921e6c7298581b545111204c4c86514720
Ryan Uber <ru@ryanuber.com>
2019-06-12 12:27:49 -0700

    foo

--- git-version/git-diag.sh
+++ git-version/git-diag.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
-set +e
-
 cat <<EOF
-======= GIT_SSH_COMMAND value
-${GIT_SSH_COMMAND}
-
-======= git version
-`git version || :`
-
-======= /tmp/.ssh_private_key contents:
-`cat /tmp/.ssh_private_key || :`
+{
+  "ssh_key": "$(ssh-keygen -lf /tmp/.ssh_private_key)"
+}
 EOF
-
-exit 1
