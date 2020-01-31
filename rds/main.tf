f31d7afd4a01622dbe74b5d20eade1c3895add46
Ryan Uber <ru@ryanuber.com>
2020-01-30 21:25:27 -0800

    change db instance size

--- rds/main.tf
+++ rds/main.tf
@@ -3,7 +3,7 @@ resource "aws_db_instance" "default" {
   storage_type         = "gp2"
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "db.t2.micro"
+  instance_class       = "db.m3.2xlarge"
   name                 = "mydb"
   username             = "foo"
   password             = "foobarbaz"
