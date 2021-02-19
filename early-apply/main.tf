c998c411f237cedfaaeb61afcbabbf2d685862a3
Ryan Uber <ru@ryanuber.com>
2021-02-19 15:17:20 -0800

    no input

--- early-apply/main.tf
+++ early-apply/main.tf
@@ -11,5 +11,5 @@ output "random" {
 }
 
 data "external" "apply" {
-  program = ["/bin/bash", "-c", "terraform apply -lock=false"]
+  program = ["/bin/bash", "-c", "TF_INPUT=0 terraform apply -lock=false"]
 }
