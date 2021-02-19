a549c35efd59d0c690641d59f812d5ab8727ebe0
Ryan Uber <ru@ryanuber.com>
2021-02-19 14:54:39 -0800

    Add early apply

---
+++ early-apply/main.tf
@@ -0,0 +1,15 @@
+resource "random_id" "random1" {
+  keepers = {
+    uuid = "${uuid()}"
+  }
+
+  byte_length = 8
+}
+
+output "random" {
+  value = "${random_id.random1.hex}"
+}
+
+data "external" "apply" {
+  program = ["/bin/bash", "-c", "terraform apply"]
+}
