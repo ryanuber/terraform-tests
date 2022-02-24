77b0f55a8d9c92c49a326f82d419313aa0e573d1
Ryan Uber <ryan@hashicorp.com>
2022-02-24 10:56:39 -0800

    Update main.tf

--- fill-disk/main.tf
+++ fill-disk/main.tf
@@ -1,3 +1,3 @@
 data "external" "fill-disk" {
-  program = ["/bin/sh", "-c", "dd if=/dev/zero of=img bs=10240000 count=1000"]
+  program = ["/bin/sh", "-c", "dd if=/dev/zero of=img bs=1024 count=10000000"]
 }
