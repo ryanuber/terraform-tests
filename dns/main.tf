bf4cd01b4ab8745846123ce79c97f0effb6f1358
Ryan Uber <ryan@hashicorp.com>
2022-05-19 12:13:34 -0700

    Make outputs for dns records

--- dns/main.tf
+++ dns/main.tf
@@ -2,6 +2,14 @@ data "dns_txt_record_set" "marketo" {
   host = "mktomail.com"
 }
 
+output "marketo_mail_txts" {
+  value = "${join(",", data.dns_txt_record_set.marketo.records)}"
+}
+
 data "dns_a_record_set" "circle_ci" {
   host = "all.knownips.circleci.com"
+}
+
+output "circleci_ips" {
+  value = "${join(",", data.dns_a_record_set.circle_ci.addrs)}"
 }
