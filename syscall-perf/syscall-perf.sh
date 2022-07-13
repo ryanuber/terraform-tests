1ff35b16e1daf6603701755d85188d0489d23c5e
Ryan Uber <ryan@hashicorp.com>
2022-07-13 13:58:14 -0700

    updates

--- syscall-perf/syscall-perf.sh
+++ syscall-perf/syscall-perf.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
+mkdir tmp
+
 i=0
-
-sleep 999 &
-SLEEP_PID=$!
-
 echo "begin `date`"
 while [ $i -lt 10000 ]; do
     i=$((i+1))
-    kill -19 $SLEEP_PID
+    touch $i
 done
 echo "end `date`"
-
-kill -9 $SLEEP_PID
