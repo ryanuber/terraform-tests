09cb934f278c777d59d9701b7666e5aa46f3ba59
Ryan Uber <ru@ryanuber.com>
2024-08-13 10:05:41 -0700

    Try harder to fill the disk

--- fill-disk/main.tf
+++ fill-disk/main.tf
@@ -1,3 +1,3 @@
 data "external" "fill-disk" {
-  program = ["/bin/sh", "-c", "dd if=/dev/zero of=img bs=1024 count=10000000"]
+  program = ["/bin/sh", "-c", "dd if=/dev/zero of=img bs=1024 count=10000000000"]
 }
